931a007de2cd120e96f47dbfbf04b96eee2aff0e
Shahjahan Jewel <cep.jewel@gmail.com>
2020-05-21 21:15:17 +0600

    Create confirm-email.js

---
+++ js-snippets/confirm-email.js
@@ -0,0 +1,15 @@
+/*
+* JS snippet to match two fields value as same
+* Example: Email and Confirm Email need to be same
+* Placement: Place the following code in your form -> Settings & Integrations -> Custom CSS/JS -> Custom Javascript BOX
+ */
+
+var $email = $form.find('input[name=email]'); // Name of the first Email Address
+var $confirmEmail = $form.find('input[name=confirm_email]'); // Name of the Confirm Email Address
+
+$confirmEmail.on('blur', function() {
+    if($(this).val() != $email.val()) {
+        alert('Email not matching');
+        // Or you can show the custom message somewhere
+    }
+});

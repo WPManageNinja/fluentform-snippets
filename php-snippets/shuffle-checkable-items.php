071e8d231dfe5a6ba7f3f26f87fb8fbec5e98720
Shahjahan Jewel <cep.jewel@gmail.com>
2020-06-25 17:18:49 +0600

    Create shuffle-checkable-items.php

---
+++ php-snippets/shuffle-checkable-items.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Shuffle the Checkable items items on render.
+ * For checkbox: fluenform_rendering_field_data_input_checkbox
+ * For Radio: fluenform_rendering_field_data_input_radio
+ * For Select: fluenform_rendering_field_data_select
+ */
+
+/*
+ * Snippet: 1
+ * This will apply for all the forms in your site
+ */
+add_filter('fluenform_rendering_field_data_input_checkbox', function ($data, $form) {
+    $options = $data['settings']['advanced_options'];
+    shuffle($options);
+    $data['settings']['advanced_options'] = $options;
+    return $data;
+}, 10, 2);
+
+
+/*
+ * Snippet: 2
+ * This will apply for only form id: 12
+ */
+
+add_filter('fluenform_rendering_field_data_input_checkbox', function ($data, $form) {
+    $targetFormId = 12;
+    if ($form->id != $targetFormId) {
+        return $data;
+    }
+    $options = $data['settings']['advanced_options'];
+    shuffle($options);
+    $data['settings']['advanced_options'] = $options;
+    return $data;
+}, 10, 2);
+

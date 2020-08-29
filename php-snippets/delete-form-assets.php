4e1137e9a5cc43fabb41df135470b8a269811bb7
Shahjahan Jewel <cep.jewel@gmail.com>
2020-08-29 20:16:11 +0600

    Update delete-form-assets.php

--- php-snippets/delete-form-assets.php
+++ php-snippets/delete-form-assets.php
@@ -1,50 +1,45 @@
-<?php
-
-/*
-* If you want to delete form entries and associate data you can use the following function
-*/
 
 function deleteFormAssests($formId)
-    {
-        // Now Let's delete associate items
-        wpFluent()->table('fluentform_submissions')
-            ->where('form_id', $formId)
-            ->delete();
+{
+    // Now Let's delete associate items
+    wpFluent()->table('fluentform_submissions')
+        ->where('form_id', $formId)
+        ->delete();
 
-        wpFluent()->table('fluentform_submission_meta')
-            ->where('form_id', $formId)
-            ->delete();
+    wpFluent()->table('fluentform_submission_meta')
+        ->where('form_id', $formId)
+        ->delete();
 
-        wpFluent()->table('fluentform_entry_details')
-            ->where('form_id', $formId)
-            ->delete();
+    wpFluent()->table('fluentform_entry_details')
+        ->where('form_id', $formId)
+        ->delete();
 
-        wpFluent()->table('fluentform_form_analytics')
-            ->where('form_id', $formId)
-            ->delete();
+    wpFluent()->table('fluentform_form_analytics')
+        ->where('form_id', $formId)
+        ->delete();
 
-        wpFluent()->table('fluentform_logs')
-            ->where('parent_source_id', $formId)
-            ->whereIn('source_type', ['submission_item', 'form_item'])
-            ->delete();
+    wpFluent()->table('fluentform_logs')
+        ->where('parent_source_id', $formId)
+        ->whereIn('source_type', ['submission_item', 'form_item'])
+        ->delete();
 
-        ob_start();
-        if (defined('FLUENTFORMPRO')) {
-            try {
-                wpFluent()->table('fluentform_order_items')
-                    ->where('form_id', $formId)
-                    ->delete();
-                wpFluent()->table('fluentform_subscriptions')
-                    ->where('form_id', $formId)
-                    ->delete();
-                wpFluent()->table('fluentform_transactions')
-                    ->where('form_id', $formId)
-                    ->delete();
-            } catch (\Exception $exception) {
+    ob_start();
+    if (defined('FLUENTFORMPRO')) {
+        try {
+            wpFluent()->table('fluentform_order_items')
+                ->where('form_id', $formId)
+                ->delete();
+            wpFluent()->table('fluentform_subscriptions')
+                ->where('form_id', $formId)
+                ->delete();
+            wpFluent()->table('fluentform_transactions')
+                ->where('form_id', $formId)
+                ->delete();
+        } catch (\Exception $exception) {
 
-            }
         }
-        $errors = ob_get_clean();
-       
-        return true;
+    }
+    $errors = ob_get_clean();
+
+    return true;
 }

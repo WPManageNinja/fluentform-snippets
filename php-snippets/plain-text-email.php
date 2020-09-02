cf73032939432fa10ffd6797bcc0216b1905e707
Shahjahan Jewel <cep.jewel@gmail.com>
2020-09-02 20:19:15 +0600

    Update plain-text-email.php

--- php-snippets/plain-text-email.php
+++ php-snippets/plain-text-email.php
@@ -11,3 +11,14 @@ add_filter('fluentform_email_body', function($body, $notification) {
     }
     return $body;
 }, 10, 2);
+
+
+// We have to remove the html flag from email header
+add_filter('fluenttform_email_header', function($headers, $notification) {
+    if(isset($notification['asPlainText']) && $notification['asPlainText'] == 'yes') {
+        if (($key = array_search('Content-Type: text/html; charset=utf-8', $headers)) !== false) {
+            unset($headers[$key]);
+        }
+    }
+    return $headers;
+}, 10, 2);
